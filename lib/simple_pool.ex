12c34f749a90b9eb4c5e6b0e442b0df08fa9613c
Andrew Cottage <andcott@gmail.com>
2017-04-07 19:59:03 -0400

    test

--- lib/simple_pool.ex
+++ lib/simple_pool.ex
@@ -6,6 +6,8 @@ defmodule SimplePool do
   @doc """
   Takes an enumerable and for every chuck it maps over the collection
   and performs the given function asynchronously
+      iex(1)> SimplePool.chunk_async([1,2,3,4,5,6,7,8,9,10], fn(n) -> n * 2 end, 3)
+      [2, 4, 6, 8, 10, 12, 14, 16, 18]
   """
   @spec chunk_async(Enum.t, (any() -> any()), integer()) :: list()
   def chunk_async(enumerable, fun, size) do
@@ -17,7 +19,7 @@ defmodule SimplePool do
   # Private Functions #
   #####################
 
-  defp do_perform([], fun, acc), do: List.flatten(acc)
+  defp do_perform([], _fun, acc), do: acc |> List.flatten() |> Enum.reverse()
   defp do_perform([chunk | rest], fun, acc) do
     tasks = do_async(chunk, fun, [])
     returns = wait_for(tasks, [])
@@ -30,7 +32,7 @@ defmodule SimplePool do
     do_async(rest, fun, [task|acc])
   end
 
-  defp wait_for([], return), do: return
+  defp wait_for([], return), do: Enum.reverse(return)
   defp wait_for([task|rest], return) do
     value = Task.await(task)
     wait_for(rest, [value|return])
--- test/simple_pool_test.exs
+++ test/simple_pool_test.exs
@@ -1,8 +1,4 @@
 defmodule SimplePoolTest do
   use ExUnit.Case
   doctest SimplePool
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
